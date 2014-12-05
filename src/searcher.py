dbd3f0fb79cd1917cc571cde4b14466a3968e18b
Danny Gratzer <jozefg@cmu.edu>
2014-12-05 00:16:27 -0500

    Fixed up searcher

--- src/searcher.py
+++ src/searcher.py
@@ -2,27 +2,29 @@
 from subprocess import call
 
 def run_with(n):
-    exit_code = call(["./solar.py", str(n)])
+    exit_code = call(["./test_search.py", str(n)])
     return False if exit_code == 0 else True
 
-current = 100
+current = 10
 
 max_value = None
-min_value = 0
+min_value = None
 
 # Establish our upper bound
 while not max_value:
     if run_with(current):
         max_value = current
+        min_value = current / 10
     else:
         current *= 10
 
-while not (-10 < max_value - min_value < 10):
-    print current
+while min_value + 1 < max_value:
     current = (max_value + min_value) / 2
+    print current
     if run_with(current):
         max_value = current
     else:
         min_value = current
+
 
 print max_value
